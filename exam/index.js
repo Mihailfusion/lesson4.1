f35992c066b378d17eb8a0c308df6a89e4221756
Mihailfusion <mihailfusion@gmail.com>
2019-03-06 20:26:00 +0300

    Рабочее состояние

--- exam/index.js
+++ exam/index.js
@@ -1,10 +1,28 @@
+// function getFriendlyNumbers(start, end) {
+//     let sumArr = [];
+
+//     for (let i = start; i <= end; i++) {
+//         let sum = 0;
+
+//         for (let j = 0; j <= i; j++) {
+//             if (i % j === 0) sum = j;
+//         }
+
+//         sumArr.push(sum);
+//     }
+
+//     return sumArr;
+   
+// }
+// getFriendlyNumbers(0, 10);
+// console.log(getFriendlyNumbers(0, 10));
+
+
 function getFriendlyNumbers(start, end) {
-    if (start > end && typeof start === 'string' && typeof end === 'string' && start % 1 == 0 && end % 1 == 0) {
+    if (start > end && typeof start === 'string' && typeof end === 'string' && start % 1 == 0 && end % 1 == 0 ) {
         var temp = start;
         start = end;
         end = temp;
-    }else {
-        // return false;
     }
     result = new Array();
     var index = 0;
@@ -21,10 +39,9 @@ function getFriendlyNumbers(start, end) {
         index++;
     }
     return result;
-    
 }
-getFriendlyNumbers(10, 100);
-
+getFriendlyNumbers(1, 1211);
+// console.log(getDivisorsRange(1, 1211));
 let arr = {
     number: [],
     sum: [],
@@ -39,29 +56,34 @@ for (var d = 0; d < result.length; d++) {
     arr.sum[d] = p;
     arr.number[d] = result[d].num;
 }
+// console.log(arr);
 for (let i = 0; i < arr.number.length; i++) {
     next:
     for (let z = 0; z < arr.sum.length; z++) {
 
-        if (arr.number[i] == arr.sum[z] && arr.sum[i] == arr.number[z] && arr.number[i] != arr.number[z]) {
+        if (arr.number[i] == arr.sum[z] && arr.sum[i] == arr.number[z] && arr.number[i] != arr.number[z]){
             answer.push([arr.number[i], arr.number[z]]);
         } else {
             continue next;
         }
     }
-}
+} 
 for (let i = 0; i < answer.length;) {
-    for (let j = 1; j < 3; j++) {
+    for (let j = 1 ; j < 3; j++) {
         if (answer[i][0] == answer[i + 1][1]) {
-            delete answer[j];
-            i++;
-            i++;
-        } else {
-        }
+        delete answer[j];
+        // console.log('ok');
+          
+        i++;
+        i++;
+        // console.log(i);  
+    }else {     
     }
 }
+}
 if (answer.length < 1) {
-    answer.push([]);
+    answer.push([])
+    
 }
 answer.sort(function (a, b) { return a - b });
-console.log(answer);
+ console.log(answer);
